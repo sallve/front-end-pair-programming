fce75e8f3e460e43d1a834cdc53fbf2d198b2e8b
Mateus Santana <mateuss.dev@gmail.com>
2020-07-24 13:36:46 -0300

    adds initial setup to pair programming

--- src/App.js
+++ src/App.js
@@ -1,24 +1,19 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+
+import logo from './assets/logo.gif';
+
+import './styles/app.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className="app">
+      <header className="header">
+        <img className="header__logo" alt="logo" src={logo} />
       </header>
+      <section className="container">
+        <aside className="products"></aside>
+        <aside className="cart"></aside>
+      </section>
     </div>
   );
 }
